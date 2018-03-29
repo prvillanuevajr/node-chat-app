9de127e9588c6962b0fcd52bfe4a971dbaba7e1b
Presmelito <prvillanuevajr@gmail.com>
2018-03-29 14:37:37 +0800

    initial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const publicPath = path.join(__dirname + '/../public');
+const express = require('express');
+const app = express();
+const port = procces.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get('/',(req,res) => {
+  res.render('index.html')
+});
+app.listen(port,() => {
+  console.log('Connected to server' + port);
+});
